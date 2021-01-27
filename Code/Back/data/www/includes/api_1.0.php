55f9ea9c7106570378ba5cacd15e8fc0dc90fba0
Maxime <maxime.sidoit@etudiant.univ-rennes1.fr>
2021-01-27 15:53:46 +0000

    Derniere modif joueur.php et ajout caserne.php

--- Code/Back/data/www/includes/api_1.0.php
+++ Code/Back/data/www/includes/api_1.0.php
@@ -16,6 +16,8 @@ require_once '1.0/connection.php';
 require_once '1.0/liste-troupes.php';
 require_once '1.0/troupes-joueur.php';
 require_once '1.0/composition-deck.php';
+require_once '1.0/joueur.php';
+require_once '1.0/caserne.php';
 
 $app->get('/', function ($req, $resp) {
 	return buildResponse($resp, 'Welcome to Korrigans API!');
