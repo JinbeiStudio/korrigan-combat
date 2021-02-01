d4fb767e5169fb4d63ec1dfd873b9a37e4632867
Florian Sautejeau <florian.sautejeau@gmail.com>
2021-02-01 08:38:40 +0000

    structure app.js

--- Code/Front/front-korrigans/src/App.js
+++ Code/Front/front-korrigans/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import Troupes from './deck/Troupes';
+import useTheme from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './App.css';
-
+/*
+  TODO :
+  npm install @material-ui/core
+*/
 function App() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  const css = {};
+
+  if(matches) {
+    css = {
+      width: '100vw'
+    }
+  } else {
+    css = {
+      width: '25em'
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="screen" style={css}>
+      <Troupes />
     </div>
   );
 }
---
+++ Code/Front/front-korrigans/src/deck/Troupes.js
@@ -0,0 +1,9 @@
+const Troupes = () => {
+    return (
+        <div>
+            <p>Liste des troupes disponibles</p>
+        </div>
+    );
+};
+
+export default Troupes;
