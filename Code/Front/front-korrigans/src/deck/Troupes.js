47ab2e3f4d5f13cd84649d34d90aa5981956fc31
Florian Sautejeau <florian.sautejeau@gmail.com>
2021-02-01 11:21:31 +0000

    troupes deck front

--- Code/Front/front-korrigans/src/deck/Troupes.js
+++ Code/Front/front-korrigans/src/deck/Troupes.js
@@ -1,4 +1,24 @@
 const Troupes = () => {
+    
+    fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/login?login=korrigans&password=korrigans&ver=1.0")
+        .then(res => console.log(res));
+    
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Access-Control-Allow-Origin', '*');
+
+    fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/joueur/1", {
+            mode: 'cors',
+            method:'GET',
+            headers: myHeaders
+        })
+        .then(res => res.json())
+        .then(
+            result => {
+                console.log(result);
+            }
+        );
+
     return (
         <div>
             <p>Liste des troupes disponibles</p>
