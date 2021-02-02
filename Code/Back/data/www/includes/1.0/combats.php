e484b992e394576e6388fb269c4f2766be6d606d
JinbeiStudio <julien.gabriel@me.com>
2021-02-02 10:43:16 +0000

    Liste des combats du joueur Web service-Julien G.

---
+++ Code/Back/data/www/includes/1.0/combats.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace combats;
+
+$app->get('/api/1.0/combats/{idJoueur}', function ($req, $resp, $args) {
+    try {
+        //global $__player_id;
+
+        /** SECURITY CHECK - MANDATORY */
+        $pdo = getPDO();
+        if (!checkToken()) {
+            return $resp->withStatus(401);   // Unauthorized
+        }
+        /** END OF SECURITY CHECK */
+
+        $stmt = $pdo->prepare('SELECT `idCombat`, `idAttaquant`, `idDefenseur`, `dateCombat`, `gagnant`, `gold` FROM `combats`');
+        $stmt->execute();
+
+        $items = [];
+        while ($row = $stmt->fetchObject()) {
+            $items[] = [
+                'idCombat' => $row->idCombat,
+                'idAttaquant' => $row->idAttaquant,
+                'idDefenseur' => $row->idDefenseur,
+                'dateCombat' => $row->dateCombat,
+                'gagnant' => $row->gagnant,
+                'gold' => $row->gold,
+            ];
+        }
+        $ret = array(
+            'listeCombats' => (array) $items,        // Cast as array for security
+        );
+        return buildResponse($resp, $ret);
+    } catch (Exception $e) {
+        __logException('Erreur lors de la récupération des combats du joueur', $e);
+        return $resp->withStatus(500);   // Internal Server Error
+    }
+});
