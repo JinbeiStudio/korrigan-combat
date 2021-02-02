93bffc7498be70223a458b7d4925995f148677c9
Florian Sautejeau <florian.sautejeau@gmail.com>
2021-02-02 09:52:56 +0000

    get cookies

--- Code/Front/front-korrigans/src/deck/Troupes.js
+++ Code/Front/front-korrigans/src/deck/Troupes.js
@@ -1,17 +1,22 @@
+import Cookies from 'js-cookie';
+
 const Troupes = () => {
     
     fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/login?login=korrigans&password=korrigans&ver=1.0")
-        .then(res => console.log(res));
-    
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Access-Control-Allow-Origin', '*');
+        .then(res => res.json())
+        .then(data => console.log(data));
+
+    let cookie = Cookies.get('kortok');
+    console.log(cookie);
 
     fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/joueur/1", {
-            mode: 'cors',
-            method:'GET',
-            headers: myHeaders
-        })
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+            },
+            credentials: 'same-origin'
+            }
+        )
         .then(res => res.json())
         .then(
             result => {
