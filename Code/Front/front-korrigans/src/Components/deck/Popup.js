66508dcdc38bd3c663c17ad03ad89a0a07ee1c3d
Florian Sautejeau <florian.sautejeau@gmail.com>
2021-02-05 15:09:23 +0000

    maj popup

--- Code/Front/front-korrigans/src/Components/deck/Popup.js
+++ Code/Front/front-korrigans/src/Components/deck/Popup.js
@@ -1,19 +1,9 @@
 import './Popup.css';
 import IconTroupe from './IconTroupe';
 
-const Popup = ({ popupOpen, onPopupClick, infos }) => {
+const Popup = ({ popupOpen, onPopupClick, infos, stats }) => {
 
     const title_bar_popup = "/images/title_bar.png";
-
-    const troupes = {"listeTroupes":
-                        [{"idTroupe":"1",
-                          "nomTroupe":"Soldat",
-                          "niveauDebloquage":"1",
-                          "poids":"1",
-                          "tempsFormation":"0.25",
-                          "capaciteTransport":"1",
-                          "portee":"1",
-                          "vitesse":"1"},{"idTroupe":"2","nomTroupe":"Archer","niveauDebloquage":"1","poids":"2","tempsFormation":"0.5","capaciteTransport":"1","portee":"2","vitesse":"1"},{"idTroupe":"3","nomTroupe":"Lancier","niveauDebloquage":"1","poids":"2","tempsFormation":"0.5","capaciteTransport":"1","portee":"2","vitesse":"1"},{"idTroupe":"4","nomTroupe":"Aigle","niveauDebloquage":"1","poids":"3","tempsFormation":"3","capaciteTransport":"1","portee":"2","vitesse":"3"},{"idTroupe":"5","nomTroupe":"Ours","niveauDebloquage":"1","poids":"4","tempsFormation":"3","capaciteTransport":"1","portee":"1","vitesse":"1"},{"idTroupe":"6","nomTroupe":"Araign\u00e9e","niveauDebloquage":"1","poids":"4","tempsFormation":"10","capaciteTransport":"1","portee":"2","vitesse":"2"},{"idTroupe":"7","nomTroupe":"Dragon","niveauDebloquage":"1","poids":"5","tempsFormation":"13","capaciteTransport":"1","portee":"3","vitesse":"1"},{"idTroupe":"8","nomTroupe":"Lanceur de Foudre","niveauDebloquage":"1","poids":"3","tempsFormation":"15","capaciteTransport":"1","portee":"3","vitesse":"1"}]};
 
     return (
         popupOpen && (<>
@@ -31,9 +21,9 @@ const Popup = ({ popupOpen, onPopupClick, infos }) => {
                 className="popup-inner">
                 <div>
                     <img className="title-bar" src={title_bar_popup} />
-                    <h2>{}</h2>
+                    <h2 className="troupe-name">{stats.nomTroupe}</h2>
                 </div>
-                <IconTroupe level={infos.niveauTroupe} troupe={infos.idTroupe} />
+                <IconTroupe className="troupe-icon" level={infos.niveauTroupe} troupe={infos.idTroupe} />
             </div>
         </>)
     );
--- Code/Front/front-korrigans/src/Components/deck/TroupesList.js
+++ Code/Front/front-korrigans/src/Components/deck/TroupesList.js
@@ -5,16 +5,31 @@ import Popup from './Popup';
 
 
 const TroupesList = ({ troupes }) => {
+    const all_troupes = {"listeTroupes":
+                        [{"idTroupe":"1",
+                          "nomTroupe":"Soldat",
+                          "niveauDebloquage":"1",
+                          "poids":"1",
+                          "tempsFormation":"0.25",
+                          "capaciteTransport":"1",
+                          "portee":"1",
+                          "vitesse":"1"},{"idTroupe":"2","nomTroupe":"Archer","niveauDebloquage":"1","poids":"2","tempsFormation":"0.5","capaciteTransport":"1","portee":"2","vitesse":"1"},{"idTroupe":"3","nomTroupe":"Lancier","niveauDebloquage":"1","poids":"2","tempsFormation":"0.5","capaciteTransport":"1","portee":"2","vitesse":"1"},{"idTroupe":"4","nomTroupe":"Aigle","niveauDebloquage":"1","poids":"3","tempsFormation":"3","capaciteTransport":"1","portee":"2","vitesse":"3"},{"idTroupe":"5","nomTroupe":"Ours","niveauDebloquage":"1","poids":"4","tempsFormation":"3","capaciteTransport":"1","portee":"1","vitesse":"1"},{"idTroupe":"6","nomTroupe":"Araign\u00e9e","niveauDebloquage":"1","poids":"4","tempsFormation":"10","capaciteTransport":"1","portee":"2","vitesse":"2"},{"idTroupe":"7","nomTroupe":"Dragon","niveauDebloquage":"1","poids":"5","tempsFormation":"13","capaciteTransport":"1","portee":"3","vitesse":"1"},{"idTroupe":"8","nomTroupe":"Lanceur de Foudre","niveauDebloquage":"1","poids":"3","tempsFormation":"15","capaciteTransport":"1","portee":"3","vitesse":"1"}]};
+
     const [popupState, setPopupState] = useState(false);
     const [popupTroupeState, setpopupTroupeState] = useState(troupes[0]);
+    const [statistiquesState, setStatistiqueState] = useState(all_troupes.listeTroupes[0]);
 
     const togglePopup = (idTroupe) => {
         setPopupState(!popupState);  
         setpopupTroupeState(troupes.find(element => element.idTroupe == idTroupe));
+
+        setStatistiqueState(all_troupes.listeTroupes.find(element => element.idTroupe == idTroupe));
+        console.log(statistiquesState);
     }
+
     const index = 0;
     return [
-        <Popup key={index} popupOpen={popupState} onPopupClick={togglePopup} infos={popupTroupeState} />,
+        <Popup key={index} popupOpen={popupState} onPopupClick={togglePopup} infos={popupTroupeState} stats={statistiquesState} />,
         troupes.map(data => {
             return <IconTroupe onTroupeClick={togglePopup} key={data.idTroupeJoueur} level={data.niveauTroupe} troupe={data.idTroupe} />
         })
