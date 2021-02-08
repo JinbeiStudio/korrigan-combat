d1d6d3a56e25aaaba455a852ace263181556fd3b
sautejfi <florian.sautejeau@gmail.com>
2021-02-08 18:51:21 +0000

    resolve pb nom dossier

--- Code/Front/front-korrigans/src/App.js
+++ Code/Front/front-korrigans/src/App.js
@@ -2,7 +2,7 @@ import Troupes from './Components/Troupe/Troupes';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import NavFooter from './Components/Footer/NavFooter';
-import Deck from './Components/Deck/Deck';
+import Deck from './Components/Decks/Deck';
 
 function App() {
   return (
