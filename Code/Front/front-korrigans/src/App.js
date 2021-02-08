8141ed9e17e862c5d153985b2d7b2dac4bf059cf
Florian Sautejeau <florian.sautejeau@gmail.com>
2021-02-08 10:13:00 +0000

    ajout background image

--- Code/Front/front-korrigans/src/App.js
+++ Code/Front/front-korrigans/src/App.js
@@ -5,11 +5,15 @@ import NavFooter from './Components/Footer/NavFooter';
 
 function App() {
   return (
+    <>
+      <div className="background" style={{ background: `url('${process.env.PUBLIC_URL}/images/background.jpg')` }}>
+      </div>
       <div className="screen">
             <Navbar />
             <Troupes />
             <NavFooter />
       </div>
+    </>
   );
 }
 
