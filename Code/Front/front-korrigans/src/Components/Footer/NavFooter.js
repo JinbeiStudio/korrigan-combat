2364eb4d8c07e8eab1ee9958bc4ea06dd391dcb3
Romain Cochin <romain.cochin.01@gmail.com>
2021-02-08 09:09:40 +0000

    update footer

--- Code/Front/front-korrigans/src/Components/Footer/NavFooter.js
+++ Code/Front/front-korrigans/src/Components/Footer/NavFooter.js
@@ -5,11 +5,11 @@ class NavFooter extends React.Component{
     render(){
         return (
             <footer>
-                <div className="nav-bloc"><a href="#"><img src=""/></a></div>
-                <div className="nav-bloc"><a href="#"><img src=""/></a></div>
-                <div className="nav-bloc"><a href="#"><img src="/images/glob_nobility-icon.png"/></a></div>
+                <div className="nav-bloc"><a href="#"><img src="/images/glob_icon-notif-build.png" className="icon-build"/></a></div>
+                <div className="nav-bloc"><a href="#"><img src="/images/map_attack-icon.png" className="icon-attack"/></a></div>
+                <div className="nav-bloc"><a href="#"><img src="/images/glob_nobility-icon.png" className="icon-helmet"/></a></div>
                 <div className="nav-bloc"><a href="#"><img src="/images/store_battle-backpack.png" className="icon-backpack"/></a></div>
-                <div className="nav-bloc"><a href="#"><img src="" className=""/></a></div>
+                <div className="nav-bloc"><a href="#"><img src="/images/glob_minirare-skillartifact.png" className="icon-artifact"/></a></div>
             </footer>
         );
     }
