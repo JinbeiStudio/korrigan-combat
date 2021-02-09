ad072b587c5685e4972aa4bbc46e5cc574919feb
Romain Cochin <romain.cochin.01@gmail.com>
2021-02-09 10:35:37 +0000

    add routes

--- Code/Front/front-korrigans/src/App.js
+++ Code/Front/front-korrigans/src/App.js
@@ -3,19 +3,35 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import NavFooter from './Components/Footer/NavFooter';
 import Deck from './Components/Decks/Deck';
+import Opponents from './Components/Opponents/Opponents'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <>
       <div className="background" style={{ background: `url('${process.env.PUBLIC_URL}/images/background.jpg')` }}>
       </div>
       <div className="screen">
             <Navbar />
-            <Deck />
-            <Troupes />
+            <Switch>
+              <Route path="/">
+                <Deck />
+                <Troupes />
+              </Route>
+              <Route path="/Opponents">
+                <Opponents />
+              </Route>
+            </Switch> 
             <NavFooter />
       </div>
     </>
+    </Router>
   );
 }
 
