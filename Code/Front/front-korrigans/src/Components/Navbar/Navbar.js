7742e322355a4c9b66db1c5470a4ed3695a6a39f
Romain Cochin <romain.cochin.01@gmail.com>
2021-03-24 15:34:49 +0000

    Use api data for the navbar

--- Code/Front/front-korrigans/src/Components/Navbar/Navbar.js
+++ Code/Front/front-korrigans/src/Components/Navbar/Navbar.js
@@ -3,62 +3,44 @@ import Clock from './Clock.js';
 import './Navbar.css';
 
 class Navbar extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            gold: "",
-            xp: "" 
-        }
+    state = {
+      loading: true,
+      joueur: null
+    };
+
+    async componentDidMount() {
+      const url = "https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/joueur/1";
+      const response = await fetch(url, {credentials: 'include'});
+      const data = await response.json();
+      this.setState({joueur: data.joueur[0]})
     }
-    /*componentDidMount() {
-      fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/login?login=korrigans&password=korrigans&ver=1.0", {credentials: 'same-origin'})
-      .then(fetch("https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/joueur/1",{credentials: 'same-origin'})
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              gold: result.gold,
-              xp: result.xp
-            });
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-      )
-      
-    }*/
     render() {
-    /*const { error, isLoaded, gold, xp } = this.state;
-    if (error) {
-      return <div>Erreur : {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Chargement…</div>;
-    } else {*/
       return (
         <nav className="NavbarItems">
           <Clock />
           <div className="bloc-nav-elements">
-            <div className="bloc-gold">
-              <img src="/images/glob_mini-gold.png" className="icon-gold"/>
-              <p>73,803</p>
-            </div>
-            <div className="bloc-ligue-point">
-              <img src="/images/glob_mini-crown-power.png" className="icon-ligue"/>
-              <p>1340</p>
-            </div>
+            {!this.state.joueur ? (
+              <div>Chargement..</div>
+            ) : (
+              <div className="bloc-gold">
+                <img src="/images/glob_mini-gold.png" className="icon-gold"/>
+                <p>{this.state.joueur.gold}</p>
+              </div>
+            )}
+            {!this.state.joueur ? (
+              <div>Chargement..</div>
+            ) : (
+              <div className="bloc-ligue-point">
+                <img src="/images/glob_mini-crown-power.png" className="icon-ligue"/>
+                <p>{this.state.joueur.xp}</p>
+              </div>          
+            )}
             <progress className="bloc-xp" max="100" value="70" />
           </div>
         </nav>
       );
     }
-  }
-
+}
 
 export default Navbar;
+
