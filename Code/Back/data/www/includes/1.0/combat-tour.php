bf4b247a16052f4a4c61a3aef121df518c7f582a
JinbeiStudio <julien.gabriel@me.com>
2021-03-24 16:26:39 +0000

    tour 2 combat

--- Code/Back/data/www/includes/1.0/combat-tour.php
+++ Code/Back/data/www/includes/1.0/combat-tour.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace combatTour1;
+namespace combatTour;
 
 DEFINE("BONUS_AGILITE", 0.3);
 DEFINE("RANDOM_MALUS", 2);
@@ -167,7 +167,7 @@ $app->get('/api/1.0/combat-tour1/attaquant/{idJoueur}/defenseur/{idDefenseur}', 
         }
         /* -------------------------------------------------------------------------- */
 
-        //Si il y a un gagant on ajout l'or et l'expérience
+        //Si il y a un gagant on ajoute l'or et l'expérience
         if ($idGagnant != NULL) {
             //Ajout de l'xp et l'or au gagant
             $stmt = $pdo->prepare('UPDATE players SET gold = gold + :orGagne, xp= xp + :experienceGagne WHERE id=:idGagnant');
@@ -206,6 +206,239 @@ $app->get('/api/1.0/combat-tour1/attaquant/{idJoueur}/defenseur/{idDefenseur}', 
     } catch (Exception $e) {
         $pdo->rollback();
         __logException('Erreur lors du calcul du tour 1', $e);
+        return $resp->withStatus(500);   // Internal Server Error
+    }
+});
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------- Combat Tour 2 --------------------------------------- */
+/* Endpoint : /api/1.0/combat-tour2/attaquant/{idJoueur}/defenseur/{idDefenseur} */
+/* ------------------------------ Method : GET --------------------------------- */
+/* ------------------------- Auteur : Julien Gabriel --------------------------- */
+$app->get('/api/1.0/combat-tour2/{idCombat}', function ($req, $resp, $args) {
+    global $__player_id;
+
+    $idCombat = $args['idCombat'];
+    $players = getPlayers($idCombat);
+
+    $checkCombatExist = checkCombatExist($idCombat);
+
+    if ($checkCombatExist == FALSE) {
+        __log('Combat inexistant');
+        return $resp->withStatus(400);
+    }
+
+    if ($players == FALSE) {
+        __log('Impossible de trouver les joueurs');
+        return $resp->withStatus(400);
+    }
+
+    $idJoueur = $players[0]['idAttaquant'];
+    $idDefenseur = $players[0]['idDefenseur'];
+
+    //Deck des joueurs
+    $deckAttaquant = getDeck($idJoueur, DECK_ATTAQUE, DECK_ACTIF);
+    $deckDefenseur = getDeck($idDefenseur, DECK_DEFENSE, DECK_ACTIF);
+
+    /* ---------------------- Si le deck est vide on annule --------------------- */
+    if (empty($deckAttaquant) || $deckAttaquant === FALSE) {
+        __log('Le deck du joueur attaquant est vide');
+        return $resp->withStatus(400);
+    }
+
+    if (empty($deckDefenseur) || $deckDefenseur === FALSE) {
+        __log('Le deck du joueur défenseur est vide');
+        return $resp->withStatus(400);
+    }
+    /* -------------------------------------------------------------------------- */
+
+
+    // Liste des stats des unités pour chaque joueur
+    $statsAttaquant = statsDeck($deckAttaquant);
+    $statsDefenseur = statsDeck($deckDefenseur);
+
+
+    /* ---------- Si erreur lors de la récupération des stats on annule --------- */
+    if (empty($statsAttaquant) || $statsAttaquant === FALSE) {
+        __log('Erreur lors de la récupération des stats de l\'attaquant');
+        return $resp->withStatus(400);
+    }
+
+    if (empty($statsDefenseur) || $statsDefenseur === FALSE) {
+        __log('Erreur lors de la récupération des stats du défenseur');
+        return $resp->withStatus(400);
+    }
+    /* -------------------------------------------------------------------------- */
+
+    //Total pour chaque stats pour l'attaquant
+    $totalDegatAttaquant = totalStats($statsAttaquant, "degat");
+    $totalAgiliteAttaquant = totalStats($statsAttaquant, "agilite");
+
+    //Total pour chaque stats pour le défenseur
+    $totalDegatDefenseur = totalStats($statsDefenseur, "degat");
+    $totalAgiliteDefenseur = totalStats($statsDefenseur, "agilite");
+
+    //Attaque des joueurs avec retour des unités restantes
+    $attaquerAttaquant = attaque($statsAttaquant, $totalDegatDefenseur, $totalAgiliteAttaquant);
+    $attaquerDefenseur = attaque($statsDefenseur, $totalDegatAttaquant, $totalAgiliteDefenseur);
+
+    //return buildResponse($resp, $attaquerAttaquant);
+
+    // Or éventuellement gagné pour chaque joueur
+    $orAttaquantTour2 = orGagne($attaquerAttaquant);
+    $orDefenseurTour2 = orGagne($attaquerDefenseur);
+
+    // Or gagné au précédent tour
+    $statsTour1 = statsTour1($idJoueur, $idDefenseur, $idCombat);
+
+    if ($statsTour1 == FALSE) {
+        __log('Or et expérience du tour 1 introuvable');
+        return $resp->withStatus(400);
+    }
+
+    $orAttaquantTour1 = $statsTour1[0]["goldAttaquant"];
+    $orDefenseurTour1 = $statsTour1[0]["goldDefenseur"];
+
+    //Or total gagné pour l'attaquant et le défenseur
+    $orAttaquant = $orAttaquantTour1 + $orAttaquantTour2;
+    $orDefenseur = $orDefenseurTour1 + $orDefenseurTour2;
+
+    //Expérience gagnée au précédent tour
+
+    //Expérience gagnée au tour 1 pour le joueur
+    $experienceAttaquantTour1 = $statsTour1[0]["experienceAttaquant"];
+    $experienceDefenseurTour1 = $statsTour1[0]["experienceDefenseur"];
+
+    //Experience éventuellement gagné au tour 2 pour le joueur
+    $experienceAttaquantTour2 = experienceGagne($attaquerDefenseur);
+    $experienceDefenseurTour2 = experienceGagne($attaquerAttaquant);
+
+    //Expérience totale gagnée
+    $experienceAttaquant = $experienceAttaquantTour1 + $experienceAttaquantTour2;
+    $experienceDefenseur = $experienceDefenseurTour1 + $experienceDefenseurTour2;
+
+    //Détermine la gagnant au deuxième tour
+    if ($attaquerAttaquant['perdu'] === true || $attaquerAttaquant['perdu'] === true && $attaquerDefenseur['perdu'] === true) {
+        $idGagnant = $idDefenseur;
+        $idPerdant = $idJoueur;
+        $orGagne = $orDefenseur;
+        $experienceGagne = $experienceDefenseur;
+    } elseif ($attaquerAttaquant['perdu'] === false && $attaquerDefenseur['perdu'] === true) {
+        $idGagnant = $idJoueur;
+        $idPerdant = $idDefenseur;
+        $orGagne = $orAttaquant;
+        $experienceGagne = $experienceAttaquant;
+    } elseif ($attaquerAttaquant['perdu'] === false && $attaquerDefenseur['perdu'] === false) {
+        $idGagnant = $idDefenseur;
+        $idPerdant = $idJoueur;
+        $orGagne = $orDefenseur;
+        $experienceGagne = $experienceDefenseur;
+    }
+
+    //return buildResponse($resp, $attaquerAttaquant);
+
+    try {
+        /** SECURITY CHECK - MANDATORY */
+        $pdo = getPDO();
+
+        if (!checkToken()) {
+            return $resp->withStatus(401);   // Unauthorized
+        }
+        /** END OF SECURITY CHECK */
+
+        /* -------------------------------------------------------------------------- */
+        /*                           Début de la transaction                          */
+        /* -------------------------------------------------------------------------- */
+        $pdo->beginTransaction();
+
+        /* ----------------- Mise à jour du combat dans la table ---------------- */
+        $stmt = $pdo->prepare('UPDATE combats SET gagnant = :gagnant, goldAttaquant = :orAttaquant, goldDefenseur = :orDefenseur, experienceAttaquant = :experienceAttaquant, experienceDefenseur = :experienceDefenseur WHERE idCombat = :idCombat');
+        $stmt->execute(["orAttaquant" => $orAttaquant, "orDefenseur" => $orDefenseur, "gagnant" => $idGagnant, "experienceAttaquant" => $experienceAttaquant, "experienceDefenseur" => $experienceDefenseur, "idCombat" => $idCombat]);
+        /* -------------------------------------------------------------------------- */
+
+        /* ----------- Insertions des résultats du tour 2 pour l'attaquant ---------- */
+        foreach ($attaquerAttaquant as $key => $value) {
+            if ($key != "perdu") {
+                $stmt = $pdo->prepare('INSERT INTO resultatsTours (idCombat, numeroTour, idTroupeJoueur, quantitePerdues) VALUES (:idCombat, 2, :idTroupeJoueur, :quantitePerdues)');
+                $stmt->execute(["idCombat" => $idCombat, "idTroupeJoueur" => $key, "quantitePerdues" => $value['quantitePerdue']]);
+            }
+        }
+        /* -------------------------------------------------------------------------- */
+
+        /* ----------- Insertion des résultats du tour 2 pour le défenseur ---------- */
+        foreach ($attaquerDefenseur as $key => $value) {
+            if ($key != "perdu") {
+                $stmt = $pdo->prepare('INSERT INTO resultatsTours (idCombat, numeroTour, idTroupeJoueur, quantitePerdues) VALUES (:idCombat, 2, :idTroupeJoueur, :quantitePerdues)');
+                $stmt->execute(["idCombat" => $idCombat, "idTroupeJoueur" => $key, "quantitePerdues" => $value['quantitePerdue']]);
+            }
+        }
+        /* -------------------------------------------------------------------------- */
+
+        /* ------- Modification des quantités de troupe du deck de l'attaquant ------ */
+        foreach ($attaquerAttaquant as $key => $value) {
+            if ($key != "perdu") {
+                if ($value['quantite'] > 0) {
+                    $stmt = $pdo->prepare('UPDATE compositionDeck SET quantite = :quantite WHERE idDeck=:idDeck AND idTroupeJoueur=:idTroupeJoueur');
+                    $stmt->execute(["quantite" => $value['quantite'], "idDeck" => $deckAttaquant[0]['idDeck'], "idTroupeJoueur" => $key]);
+                } else {
+                    $stmt = $pdo->prepare('DELETE FROM compositionDeck WHERE idDeck=:idDeck AND idTroupeJoueur=:idTroupeJoueur');
+                    $stmt->execute(["idDeck" => $deckAttaquant[0]['idDeck'], "idTroupeJoueur" => $key]);
+                }
+            }
+        }
+        /* -------------------------------------------------------------------------- */
+
+        /* ------- Modification des quantités de troupe du deck du défenseur -------- */
+        foreach ($attaquerDefenseur as $key => $value) {
+            if ($key != "perdu") {
+                if ($value['quantite'] > 0) {
+                    $stmt = $pdo->prepare('UPDATE compositionDeck SET quantite = :quantite WHERE idDeck=:idDeck AND idTroupeJoueur=:idTroupeJoueur');
+                    $stmt->execute(["quantite" => $value['quantite'], "idDeck" => $deckDefenseur[0]['idDeck'], "idTroupeJoueur" => $key]);
+                } else {
+                    $stmt = $pdo->prepare('DELETE FROM compositionDeck WHERE idDeck=:idDeck AND idTroupeJoueur=:idTroupeJoueur');
+                    $stmt->execute(["idDeck" => $deckDefenseur[0]['idDeck'], "idTroupeJoueur" => $key]);
+                }
+            }
+        }
+        /* -------------------------------------------------------------------------- */
+
+        //Ajout de l'xp et l'or au gagant
+        $stmt = $pdo->prepare('UPDATE players SET gold = gold + :orGagne, xp= xp + :experienceGagne WHERE id=:idGagnant');
+        $stmt->execute(["orGagne" => $orGagne, "experienceGagne" => $experienceGagne, "idGagnant" => $idGagnant]);
+
+        //Baisse de l'or du perdant
+        $stmt = $pdo->prepare('UPDATE players SET gold = GREATEST(gold - :orGagne, 0) WHERE id=:idPerdant');
+        $stmt->execute(["orGagne" => $orGagne, "idPerdant" => $idPerdant]);
+
+
+        $pdo->commit();
+        /* -------------------------------------------------------------------------- */
+        /*                            Fin de la transaction                           */
+        /* -------------------------------------------------------------------------- */
+
+        $items = [];
+        $items['resultat'] = $attaquerAttaquant;
+        $items['resultat']['orGagne'] = $orAttaquant;
+        $items['resultat']['orPerdu'] = $orDefenseur;
+        $items['resultat']['combat'] = $idCombat;
+        if ($idGagnant == $idJoueur) {
+            $items['resultat']['gagne'] = true;
+        } else {
+            $items['resultat']['gagne'] = false;
+        }
+
+        $ret = array(
+            'resultatTour2Attaquant' => (array) $items,
+        );
+
+        if (!$ret) {
+            __log('Erreur lors du calcul du tour 2');
+            return $resp->withStatus(404);   // Not found
+        }
+        return buildResponse($resp, $ret);
+    } catch (Exception $e) {
+        $pdo->rollback();
+        __logException('Erreur lors du calcul du tour 2', $e);
         return $resp->withStatus(500);   // Internal Server Error
     }
 });
@@ -362,5 +595,65 @@ function attaque($stats, $totalDegat, $totalAgilite)
     die; */
 
     return $stats;
+}
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                               Id des joueurs                               */
+/* -------------------------------------------------------------------------- */
+function getPlayers($idCombat)
+{
+    try {
+        $pdo = getPDO();
+
+        //Id du joueur attaquant
+        $queryID = $pdo->prepare("SELECT `idAttaquant`, `idDefenseur` FROM `combats` WHERE idCombat = :idCombat");
+        $queryID->execute(["idCombat" => $idCombat]);
+        $result = $queryID->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    } catch (Exception $e) {
+        return FALSE;
+    }
+}
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                             Or gagné au tour 1                             */
+/* -------------------------------------------------------------------------- */
+function statsTour1($idAttaquant, $idDefenseur, $idCombat)
+{
+    try {
+        $pdo = getPDO();
+
+        //Id du joueur attaquant
+        $queryID = $pdo->prepare("SELECT `goldAttaquant`, `goldDefenseur`, `experienceAttaquant`, `experienceDefenseur` FROM `combats` WHERE idCombat=:idCombat");
+        $queryID->execute(["idCombat" => $idCombat]);
+        $result = $queryID->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    } catch (Exception $e) {
+        return FALSE;
+    }
+}
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*           Vérification de l'existence du combat                            */
+/* -------------------------------------------------------------------------- */
+function checkCombatExist($idCombat)
+{
+    try {
+        $pdo = getPDO();
+
+        //Id du joueur attaquant
+        $queryID = $pdo->prepare("SELECT `idCombat` FROM `combats` WHERE idCombat=:idCombat");
+        $queryID->execute(["idCombat" => $idCombat]);
+        $result = $queryID->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    } catch (Exception $e) {
+        return FALSE;
+    }
 }
 /* -------------------------------------------------------------------------- */
