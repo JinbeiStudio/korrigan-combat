c05b2c23c2880fc378df4cdaaf8d580defce6efd
Romain Cochin <romain.cochin.01@gmail.com>
2021-03-26 12:32:23 +0000

    Add opponents list data

--- Code/Front/front-korrigans/src/Components/Opponents/Opponents.js
+++ Code/Front/front-korrigans/src/Components/Opponents/Opponents.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './Opponents.css';
 
 class Opponents extends React.Component{
+    state = {
+        loading: true,
+        adversaire: []
+    };
+    async componentDidMount() {
+        const url = "https://korrigans-team2-ws.lpweb-lannion.fr/api/1.0/adversaire/5";
+        const response = await fetch(url, {credentials: 'include'});
+        const data = await response.json();
+        this.setState({adversaire: data.adversaire})
+    }
     render(){
         return (
-            <div className="bloc-adversaire">
-                <p>Pseudo Joueur</p>
-                <p>Niveau</p>
-                <button className="button-adversaire" style={{ background: `url('${process.env.PUBLIC_URL}/images/adversaires/glob_small-button2..png')` }}>Affronter</button>
+            <div>
+                {this.state.adversaire.map(i=> (
+                    <div className="bloc-adversaire">
+                        <p>{i.login}</p>
+                        <p>{i.level}</p>
+                        <button className="button-adversaire" style={{ background: `url('${process.env.PUBLIC_URL}/images/adversaires/glob_small-button2..png')` }}>Affronter</button>
+                    </div>
+                ))}
             </div>
         );
     }
