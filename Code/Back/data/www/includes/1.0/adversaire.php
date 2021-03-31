858e03e526564869ff5a414d36ef695b3feb7fdc
JinbeiStudio <julien.gabriel@me.com>
2021-03-31 09:36:30 +0000

    vérification deck défense adversaire

--- Code/Back/data/www/includes/1.0/adversaire.php
+++ Code/Back/data/www/includes/1.0/adversaire.php
@@ -3,6 +3,7 @@
 namespace adversaire;
 
 /* ----------------------- Obtenir le niveau du joueur ---------------------- */
+
 function getInfoJoueur($id)
 {
    global $__player_id;
@@ -25,7 +26,7 @@ function getInfoJoueur($id)
 /* -------------------- 5 adversaires pour un joueur ------------------------ */
 /* ------------------ Endpoint : /api/1.0/adversaire/{idJoueur} ------------- */
 /* ------------------------------ Method : GET ------------------------------ */
-/* ------------------------- Auteur : Maxime Sidoit ------------------------- */
+/* --------- Auteur : Maxime Sidoit & Julien Gabriel ------------------------ */
 $app->get('/api/1.0/adversaire/{id}', function ($req, $resp, $args) {
    global $__player_id;
 
@@ -51,6 +52,21 @@ $app->get('/api/1.0/adversaire/{id}', function ($req, $resp, $args) {
             'level' => $row->level,
          ];
       }
+
+      $checkDeckExist = checkDeckExist();
+
+      if (!$checkDeckExist) {
+         __log('Problème lors de la vérification des deck');
+         return $resp->withStatus(404);   // Not found
+      }
+
+      // On enlève du tableau les joueurs qui n'ont pas de deck de défense
+      foreach ($items as $key => $value) {
+         if (!array_keys(array_combine(array_keys($checkDeckExist), array_column($checkDeckExist, 'idJoueur')), $value['id'])) {
+            unset($items[$key]);
+         }
+      }
+
       $ret = array(
          'adversaire' => (array) $items,
       );
@@ -66,4 +82,22 @@ $app->get('/api/1.0/adversaire/{id}', function ($req, $resp, $args) {
       return $resp->withStatus(500);   // Internal Server Error
    }
 });
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*      Vérification si le joueur a des troupes dans son deck de défense      */
+/* -------------------------------------------------------------------------- */
+function checkDeckExist()
+{
+   try {
+      $pdo = getPDO();
+
+      $stmt = $pdo->prepare('SELECT `idJoueur` FROM `deck` INNER JOIN `compositionDeck` ON deck.idDeck=compositionDeck.idDeck WHERE `type`=2 AND `quantite` != 0 GROUP BY `idJoueur`');
+      $stmt->execute();
+      $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+      return $result;
+   } catch (Exception $e) {
+      return FALSE;
+   }
+}
 /* -------------------------------------------------------------------------- */
